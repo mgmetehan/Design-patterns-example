316a813f5c3936f2a29d9ebaf0ba5e31cb16e461
mgmetehan <mgmetehan@hotmail.com>
2023-02-24 20:13:06 +0300

    init commit

---
+++ Design-patterns-example/src/main/java/com/mgmetehan/Designpatternsexample/DesignPatternsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.Designpatternsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsExampleApplication.class, args);
+	}
+
+}
---
+++ Design-patterns-example/src/test/java/com/mgmetehan/Designpatternsexample/DesignPatternsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.Designpatternsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
